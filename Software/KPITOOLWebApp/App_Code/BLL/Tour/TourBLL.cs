8b03c6cfd6f1f58d77e38b614aff1517052ceaa9
Jose Carlos Gutierrez <joseco48@gmail.com>
2016-08-01 06:16:50 -0400

    Files for tour

---
+++ Software/KPITOOLWebApp/App_Code/BLL/Tour/TourBLL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Artexacta.App.Tour
+{
+
+    /// <summary>
+    /// Summary description for TourBLL
+    /// </summary>
+    public class TourBLL
+    {
+        public TourBLL()
+        {
+            
+        }
+
+        public static void SetUserTourStatus(int userId, string tourId)
+        {
+            if (userId <= 0)
+                throw new ArgumentException("UserId cannot be equals or less than zero");
+
+            if (string.IsNullOrEmpty(tourId))
+                throw new ArgumentException("TourId cannot be null or empty");
+
+            TourDSTableAdapters.QueriesTableAdapter adapter = new TourDSTableAdapters.QueriesTableAdapter();
+            adapter.SetUserTourStatus(userId, tourId);
+        }
+
+        public static bool CheckIfUserViewTour(int userId, string tourId)
+        {
+            if (userId <= 0)
+                throw new ArgumentException("UserId cannot be equals or less than zero");
+
+            if (string.IsNullOrEmpty(tourId))
+                throw new ArgumentException("TourId cannot be null or empty");
+
+            bool? status = false;
+            TourDSTableAdapters.QueriesTableAdapter adapter = new TourDSTableAdapters.QueriesTableAdapter();
+            adapter.CheckUserTourStatus(userId, tourId, ref status);
+
+            return status.Value;
+        }
+    }
+}
